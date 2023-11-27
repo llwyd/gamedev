4bd2b30e5d876f44abe1af269a4b0734af8e2ec5
llwyd <tom@llwyd.io>
2023-11-27 19:13:13 +0000

    draw triangle

--- space_clutter/src/main.rs
+++ space_clutter/src/main.rs
@@ -1,8 +1,33 @@
 use nannou::prelude::*;
 
-const WINDOW_SIZE: (f32,f32) = (640.0, 480.0);
+const WINDOW_SIZE: (u32, u32) = (640, 480);
 
-struct Model { }
+#[derive(Copy,Clone)]
+enum State{
+    Idle, // Normal game 
+//    GameOver,
+//    Menu,
+}
+
+#[derive(Copy,Clone)]
+enum StateEvents{
+    Left_KeyPress,
+    Left_KeyRelease,
+    Right_KeyPress,
+    Right_KeyRelease,
+    Up_KeyPress,
+    Up_KeyRelease,
+}
+
+struct Player{
+    position: Point2,
+    rotation: f32,
+    score: u32,
+}
+
+struct Model {
+    player: Player,
+}
 
 fn main() {
     nannou::app(model)
@@ -14,16 +39,22 @@ fn main() {
 
 fn model(app: &App) -> Model {
     app.new_window()
-        .size(640,480)
-        .min_size(640,480)
-        .max_size(640,480)
+        .size(WINDOW_SIZE.0, WINDOW_SIZE.1)
+        .min_size(WINDOW_SIZE.0, WINDOW_SIZE.1)
+        .max_size(WINDOW_SIZE.0, WINDOW_SIZE.1)
         .decorations(true)
         .resizable(false)
         .event(window_event)
         .build()
         .unwrap();
     
-    let mut model = Model { };
+    let mut model = Model {
+        player: Player {
+                position: pt2(0.0, 0.0),
+                rotation: 0.0,
+                score: 0,
+        }
+    };
 
     model
 }
@@ -38,4 +69,17 @@ fn update(app: &App, model: &mut Model, update: Update) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame){
+    let win = app.window_rect();
+    let draw = app.draw();
+    draw.background().color(BLACK);
+
+    let point1 = pt2(-15.0, 0.0);
+    let point2 = pt2(15.0, 0.0);
+    let point3 = pt2(0.0, 39.0);
+
+    draw.tri()
+        .points(point1,point2,point3)
+        .color(WHITE);
+
+    draw.to_frame(app, &frame).unwrap();
 }

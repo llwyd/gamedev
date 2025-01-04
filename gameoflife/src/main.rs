149228f1a5c4ec5426758e1b99677f9c2c8d0605
llwyd <tom@llwyd.io>
2025-01-04 15:22:32 +0000

    draw a white square

--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -5,11 +5,12 @@ fn setup(mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>){
 
     //meshes.add(Circle::new(50.0));
-    let colour = Color::hsl(0.5,0.5,0.5);
+    let square = Rectangle::new(50.0, 50.0);
+    let colour = Color::hsl(1.0,1.0,1.0);
     //materials.add(colour);
 
     commands.spawn(Camera2d);
-    commands.spawn((Mesh2d(meshes.add(Circle::new(50.0))),MeshMaterial2d(materials.add(colour)),Transform::from_xyz(0.0,0.0,0.0)));
+    commands.spawn((Mesh2d(meshes.add(square)),MeshMaterial2d(materials.add(colour)),Transform::from_xyz(0.0,0.0,0.0)));
 }
 
 fn main() {

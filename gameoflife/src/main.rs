1e54ad5e0d556c70f60c604b3bed079ce51a3a5a
llwyd <tom@llwyd.io>
2025-01-04 15:18:39 +0000

    draw a circle

--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>){
+
+    //meshes.add(Circle::new(50.0));
+    let colour = Color::hsl(0.5,0.5,0.5);
+    //materials.add(colour);
+
+    commands.spawn(Camera2d);
+    commands.spawn((Mesh2d(meshes.add(Circle::new(50.0))),MeshMaterial2d(materials.add(colour)),Transform::from_xyz(0.0,0.0,0.0)));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }

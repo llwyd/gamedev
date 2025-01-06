18cc233f16b0ca9dac6a7488d844f9ae86095e7d
llwyd <tom@llwyd.io>
2025-01-06 20:23:48 +0000

    adding some consts

--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -1,27 +1,39 @@
 use bevy::prelude::*;
 
+const WINDOW_SIZE: (f32, f32) = (640., 480.);
+const SQUARE_SIZE: f32 = 80.;
+const NUM_COLS: u32 = (WINDOW_SIZE.0 / SQUARE_SIZE) as u32;
+const NUM_ROWS: u32 = (WINDOW_SIZE.1 / SQUARE_SIZE) as u32;
+const NUM_SQUARES: u32 = NUM_COLS * NUM_ROWS;
+const ALIVE_COLOUR: Color = Color::hsl(1.0,1.0,1.0);
+const DEAD_COLOUR: Color = Color::hsl(0.5,0.5,0.5);
+
+
 fn setup(mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>){
 
-    //meshes.add(Circle::new(50.0));
-    let square = Rectangle::new(50.0, 50.0);
-    let colour = Color::hsl(1.0,1.0,1.0);
-    //materials.add(colour);
+    println!("Columns: {:}", NUM_COLS);
+    println!("   Rows: {:}", NUM_ROWS);
+    println!("Squares: {:}", NUM_SQUARES);
+
+    let square = Rectangle::new(SQUARE_SIZE, SQUARE_SIZE);
+    let colour = ALIVE_COLOUR;
 
     commands.spawn(Camera2d);
-    commands.spawn((Mesh2d(meshes.add(square)),MeshMaterial2d(materials.add(colour)),Transform::from_xyz(0.0,0.0,0.0)));
+    commands.spawn((Mesh2d(meshes.add(square)),MeshMaterial2d(materials.add(colour)),Transform::from_xyz(-320.0 + (SQUARE_SIZE / 2.0),0.0,0.0)));
 }
 
 fn main() {
     let mut app = App::new();
+    app.insert_resource(ClearColor(Color::srgb(0.0,0.0,0.0)));
     app.add_plugins(DefaultPlugins.set(WindowPlugin{
         primary_window: { 
             Some(
                 Window{
                     title: "Conway's game of life".into(),
-                    resolution: (800., 600.).into(),
-                    resize_constraints: WindowResizeConstraints{min_width: 800., min_height: 600., max_width: 800., max_height: 600.},
+                    resolution: (WINDOW_SIZE.0, WINDOW_SIZE.1).into(),
+                    resize_constraints: WindowResizeConstraints{min_width: WINDOW_SIZE.0, min_height: WINDOW_SIZE.1, max_width: WINDOW_SIZE.0, max_height: WINDOW_SIZE.1},
                     resizable: false,
                     ..default()
                 }
